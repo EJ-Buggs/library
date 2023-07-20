79737c0e8b3d6e3ff011476085ff498204124a38
ebuggs <ebuggs212@gmail.com>
2023-07-20 12:43:19 -0400

    add clear input fields, cards created with js, and diplayed objects on webpage with data index and append

--- script.js
+++ script.js
@@ -3,10 +3,21 @@ let currentIndex = 1;
 const form = document.getElementById("form");
 const addBook = document.querySelector(".add-book");
 const submit = document.querySelector(".submit");
+const inputFields = form.querySelectorAll("input");
 
-addBook.addEventListener("click", function () {
-  form.classList.remove("form");
-});
+const cardContainer = document.querySelector(".cardContainer");
+
+function addCard() {
+  const card = document.createElement("div");
+  card.classList.add(`card${currentIndex - 1}`);
+  card.classList.add("card");
+
+  const lastBook = myLibrary[myLibrary.length - 1];
+  if (lastBook) {
+    card.textContent = `Title: ${lastBook.data.title}, Author: ${lastBook.data.author}, Pages: ${lastBook.data.pages}`;
+  }
+  cardContainer.appendChild(card);
+}
 
 function handleSubmit(event) {
   event.preventDefault();
@@ -22,6 +33,12 @@ function handleSubmit(event) {
   });
 
   myLibrary.push(formDataObject);
+
+  addCard();
+
+  inputFields.forEach((input) => {
+    input.value = "";
+  });
 
   console.log(myLibrary);
 }
