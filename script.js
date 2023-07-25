8640af24e23dedf7d72ef1f5242b69cafe87d877
ebuggs <ebuggs212@gmail.com>
2023-07-25 13:04:05 -0400

    add not read and read button and remove button

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const form = document.getElementById("form");
 const addBook = document.querySelector(".add-book");
 const submit = document.querySelector(".submit");
 const inputFields = form.querySelectorAll("input");
+const checkButton = document.getElementById("readCheckbox");
+let readBtn = "";
+const cardToRemove = document.querySelector(`.card${currentIndex - 1}`);
 
 const cardContainer = document.querySelector(".cardContainer");
 
@@ -13,9 +16,48 @@ function addCard() {
   card.classList.add("card");
 
   const lastBook = myLibrary[myLibrary.length - 1];
+  const bookInfo = document.createElement("p");
   if (lastBook) {
-    card.textContent = `Title: ${lastBook.data.title}, Author: ${lastBook.data.author}, Pages: ${lastBook.data.pages}`;
+    bookInfo.textContent = `Title: ${lastBook.data.title}, Author: ${lastBook.data.author}, Pages: ${lastBook.data.pages}`;
   }
+
+  readBtn = document.createElement("button");
+  readBtn.classList.add(`button${currentIndex - 1}`);
+  readBtn.classList.add("readBtn");
+
+  const readCheckbox = document.createElement("input");
+  readCheckbox.setAttribute("type", "checkbox");
+  readCheckbox.classList.add("readCheckbox");
+
+  let isChecked = readCheckbox.checked;
+  readBtn.textContent = isChecked ? "Not read" : "Read";
+  readBtn.style.backgroundColor = isChecked ? "#ff9c9c" : "#9fff9c";
+
+  readBtn.addEventListener("click", function () {
+    isChecked = !isChecked;
+    readCheckbox.checked = isChecked;
+    readBtn.textContent = isChecked ? "Not read" : "Read";
+    readBtn.style.backgroundColor = isChecked ? "#ff9c9c" : "#9fff9c";
+  });
+
+  const removeBtn = document.createElement("button");
+  removeBtn.classList.add("remove");
+  removeBtn.classList.add(`remove${currentIndex - 1}`);
+  removeBtn.textContent = "Remove";
+
+  const cardIndex = currentIndex - 1;
+
+  removeBtn.addEventListener("click", function () {
+    const cardToRemove = document.querySelector(`.card${cardIndex}`);
+    if (cardToRemove) {
+      cardContainer.removeChild(cardToRemove);
+    }
+  });
+
+  card.appendChild(bookInfo);
+  card.appendChild(readBtn);
+  card.appendChild(removeBtn);
+  //card.appendChild(readCheckbox);
   cardContainer.appendChild(card);
 }
 
